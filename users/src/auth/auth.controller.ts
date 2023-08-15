416e1da89490d56a06099eb7431c1e93dd3bd942
temeriamos <czipo14@gmail.com>
2023-08-16 01:38:54 +0200

    feat: logout user

--- users/src/auth/auth.controller.ts
+++ users/src/auth/auth.controller.ts
@@ -9,10 +9,18 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto/auth.dto';
+import {AuthGuard} from "@nestjs/passport";
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/logout')
+  @HttpCode(HttpStatus.OK)
+  async logout(@Req() req) {
+    const userId = req.user.sub;
+    await this.authService.invalidateRefreshToken(userId);
+    return { message: 'El usuario se desconectó con éxito' };
+  }
   @Post('signin')
   @HttpCode(HttpStatus.OK)
   signIn(@Body() dto: AuthDto) {
