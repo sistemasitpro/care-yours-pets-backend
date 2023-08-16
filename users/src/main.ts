cb9a9c3090d3608925abb9152537c291d49a23c8
temeriamos <czipo14@gmail.com>
2023-08-16 12:58:29 +0200

    feat: enable cors

--- users/src/main.ts
+++ users/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import {BadRequestException, ValidationError, ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(
       new ValidationPipe({
         exceptionFactory: (validationErrors: ValidationError[] = []) => {
