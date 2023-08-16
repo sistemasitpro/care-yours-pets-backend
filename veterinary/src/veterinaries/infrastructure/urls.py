50a622ea119f901e00a2fae4a9517a48ec629093
Carlos Andres Aguirre Ariza <aguizandre.2803@gmail.com>
2023-08-16 11:06:16 -0500

    refactor(veterinary): change authentication endpoint name

--- veterinary/src/veterinaries/infrastructure/urls.py
+++ veterinary/src/veterinaries/infrastructure/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('v1/veterinary', view=Register.as_view(), name='register'),
     
     # authentication
-    path('v1/auth/login', view=Login.as_view(), name='login'),
+    path('v1/auth/signin', view=Login.as_view(), name='login'),
     path('v1/auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('v1/auth/logout/<str:id>', view=Logout.as_view(), name='logout'),
     
