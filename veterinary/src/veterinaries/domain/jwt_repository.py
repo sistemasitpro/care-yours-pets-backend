3f9120c60c4d24043afdcc0889b6a10dc422c18c
Carlos Andres Aguirre Ariza <aguizandre.2803@gmail.com>
2023-08-16 10:41:40 -0500

    docs(veterinary): add docstrings for code documentation

--- veterinary/src/veterinaries/domain/jwt_repository.py
+++ veterinary/src/veterinaries/domain/jwt_repository.py
@@ -7,6 +7,10 @@ from rest_framework_simplejwt.token_blacklist.models import (
 class Repository:
     """
     Encapsulates operations CRUD on the OutstandingToken and BlacklistedToken models.
+    
+    Attributes:
+        model_outstandingtoken (Model): The OutstandingToken model.
+        model_blacklistedtoken (Model): The BlacklistedToken model.
     """
     
     def __init__(self) -> None:
@@ -14,6 +18,16 @@ class Repository:
         self.model_blacklistedtoken = BlacklistedToken
     
     def create_outstandingtoken(self, data:dict) -> OutstandingToken:
+        """
+        Creates a new instance of the OutstandingToken model with the provided data.
+
+        Parameters:
+            data (dict): A dictionary containing the data to create a new OutstandingToken instance.
+
+        Returns:
+            OutstandingToken: The new OutstandingToken instance that has been created.
+        """
+        
         return self.model_outstandingtoken.objects.create(
             user=data['veterinary_instance'],
             jti=data['jti'],
@@ -23,11 +37,31 @@ class Repository:
         )
 
     def create_blacklistedtoken(self, access_token:OutstandingToken) -> BlacklistedToken:
+        """
+        Creates a new instance of the BlacklistedToken model with the provided OutstandingToken instance.
+
+        Parameters:
+            access_token (OutstandingToken): An instance of OutstandingToken to be blacklisted.
+
+        Returns:
+            BlacklistedToken: The new BlacklistedToken instance that has been created.
+        """
+        
         return self.model_blacklistedtoken.objects.create(
             token=access_token,
         )
     
     def get_by_only_jti(self, jti:str) -> OutstandingToken | None:
+        """
+        Gets an instance of the OutstandingToken model by its jti attribute.
+
+        Parameters:
+            jti (str): The jti of the OutstandingToken instance to retrieve.
+
+        Returns:
+            OutstandingToken | None: The OutstandingToken instance with the provided jti, or None if there is none.
+        """
+        
         return self.model_outstandingtoken.objects.filter(jti=jti).only('id').first()
 
 jwtr = Repository()
--- veterinary/src/veterinaries/domain/veterinary_repository.py
+++ veterinary/src/veterinaries/domain/veterinary_repository.py
@@ -8,12 +8,25 @@ from veterinaries.models import Veterinaries
 class Repository:
     """
     Encapsulates operations CRUD on the Veterinaries models.
+    
+    Attributes:
+        models (Model): The currently active user model.
     """
     
     def __init__(self) -> None:
         self.models = get_user_model()
     
     def create(self, data:dict) -> Veterinaries:
+        """
+        Creates a new instance of the Veterinaries model with the provided data.
+
+        Parameters:
+            data (dict): A dictionary containing the data to create a new Veterinaries instance.
+
+        Returns:
+            Veterinaries: The new Veterinaries instance that has been created.
+        """
+        
         return self.models.objects.create_veterinary(
             nif_cif=data['nif_cif'],
             name=data['name'],
@@ -26,6 +39,13 @@ class Repository:
         )
 
     def get_all(self) -> Veterinaries | None:
+        """
+        Gets all active instances of the Veterinaries model.
+
+        Returns:
+            Veterinaries | None: A list of all active Veterinaries instances, or None if there are none.
+        """
+        
         return self.models.objects.filter(is_active=True).all().only('id', 'name', 'description', 'city_id', 'address', 'phone_number')
 
 vetr = Repository()
